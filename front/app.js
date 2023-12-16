b051723c0bb6261bc39ad6f5bb0ddb40c42ab6ea
ahmmedalsaeed <109894462+ahmmedalsaeed@users.noreply.github.com>
2023-12-16 21:59:34 +0000

    last update 17/12- 1 am

--- front/app.js
+++ front/app.js
@@ -6,33 +6,80 @@ import {
   RouterProvider,
   useRouteError
 } from "react-router-dom";
-import axios from 'axios';
 
 
 function Dashboard() {
+  const [zoneData, setZoneData] = useState([]);
+  const [sensorData, setSensorData] = useState([]);
+  const [dataData, setDataData] = useState([]);
+  // GET /zones
+  useEffect(() => {
+      fetch('https://automatic-space-acorn-rqp5pw5x9rxfppr9-5000.app.github.dev/getzones')
+        .then((res) => res.json())
+        .then(res => {
+          console.log('Response Zones:', res);
+          if (res && Array.isArray(res.data)) {
+            setZoneData(res.data);
+          } else {
+            throw new Error('Invalid response format');
+          }
+        })
+        .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  // GET /sensors
+  useEffect(() => {
+    fetch('https://automatic-space-acorn-rqp5pw5x9rxfppr9-5000.app.github.dev/getsensors')
+      .then((res) => res.json())
+      .then(res => {
+        console.log('Response Sensors:', res);
+        if (res && Array.isArray(res.data)) {
+          setSensorData(res.data);
+        } else {
+          throw new Error('Invalid response format');
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  // GET /data
+  useEffect(() => {
+    fetch('https://automatic-space-acorn-rqp5pw5x9rxfppr9-5000.app.github.dev/getdata')
+      .then((res) => res.json())
+      .then(res => {
+        console.log('Response Data:', res);
+        if (res && Array.isArray(res.data)) {
+          setDataData(res.data);
+        } else {
+          throw new Error('Invalid response format');
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
     return (
             <div id="dashboard_page">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="well">
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div className="well">
                             <h4>Zones</h4>
-                            <p>3 Zones</p> 
+                            <p>{zoneData.length} Zones</p> 
                         </div>
                     </div>
-                    <div class="col-sm-4">
-                        <div class="well">
+                    <div className="col-sm-4">
+                        <div className="well">
                             <h4>Sensors</h4>
-                            <p>100 sensors</p> 
+                            <p>{sensorData.length} sensors</p> 
                         </div>
                     </div>
-                    <div class="col-sm-4">
-                        <div class="well">
+                    <div className="col-sm-4">
+                        <div className="well">
                             <h4>Readings</h4>
-                            <p>10 Million</p> 
+                            <p>{dataData.length} sensors' readings</p> 
                         </div>
                     </div>
                 </div>
-                <div class="well">
+                <div className="well">
                     <h4>Dashboard</h4>
                     <p>Some text..</p>
                 </div>
@@ -42,10 +89,8 @@ function Dashboard() {
 
 function Data() {
     const [zoneData, setZoneData] = useState([]);
-    const [loadingErr, setErr] = useState('');
-    const [loading, setLoading] = useState(true);
     useEffect(() => {
-        fetch('https://cautious-palm-tree-v9qjqwj5jv7hwgrv-5000.app.github.dev/getzones')
+        fetch('https://automatic-space-acorn-rqp5pw5x9rxfppr9-5000.app.github.dev/getzones')
           .then((res) => res.json())
           .then(res => {
             console.log('Response:', res);
@@ -67,112 +112,164 @@ function Data() {
             }
           })
           .catch(error => console.error('Error fetching data:', error));
-      }, []);
-      
-      useEffect(() => {
-        console.log('Current zoneData:', zoneData);
-      }, [zoneData]);
-    
-    const [data, setData] = useState([]); //Default value is an empty array
-    useEffect(() => {
-        fetch('https://dummyjson.com/products')
-            .then((res) => res.json())
-            .then((json_obj) => setData(json_obj.products))
-            .catch((err) => console.error(err));
     }, []);
+      
+    // useEffect(() => {
+    //   console.log('Current zoneData:', zoneData);
+    // }, [zoneData]);
 
     const [zoneId, setZoneId] = useState('');
     const [zoneName, setZoneName] = useState('');
+    const resetZoneState = () => {
+      setZoneId(''); 
+      setZoneName('');
+    };
 
     const [sensorId, setSensorId] = useState('');
     const [sensorName, setSensorName] = useState('');
     const [sensorType, setSensorType] = useState('');
     const [sensorLocation, setSensorLocation] = useState('');
+    const resetSensorState = () => {
+      setSensorId(''); 
+      setSensorName('');
+      setSensorType('');
+      setSensorLocation('');
+    };
 
     const handleZoneSubmit = (event) => {
-        event.preventDefault(); // Prevents the default form submission behavior
-    
-        // Handle form submission logic here (e.g., sending data to the server)
-        axios.post('', {zoneId,zoneName})
-        .then(result => console.log(result))
-        .catch(err => console.log(err))
+      event.preventDefault(); // Prevents the default form submission behavior
+      const zoneData = {
+        zoneId: zoneId,
+        zoneName: zoneName,
       };
-      const handleSensorSubmit = (event) => {
-        event.preventDefault(); // Prevents the default form submission behavior
+
+      fetch('https://automatic-space-acorn-rqp5pw5x9rxfppr9-5000.app.github.dev/zone', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json', // Set the Content-Type header for JSON data
+          },
+          body: JSON.stringify(zoneData), // Convert zoneData to JSON format
+        })
+      .then(response => {
+        if (response.ok) {
+          Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: "Zone has been added",
+            showConfirmButton: false,
+            timer: 1500
+          });
+          resetZoneState();
+        } else {
+          
+          throw new Error('Failed to post zone data');
+        }
+      })
+      .catch(error => {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: error.message,
+          confirmButtonColor: "#1f629c",
+        });
+      });
+    };
     
-        // Handle form submission logic here (e.g., sending data to the server)
-        axios.post('', {sensorId,sensorName,sensorLocation})
-        .then(result => console.log(result))
-        .catch(err => console.log(err))
+    const handleSensorSubmit = (event) => {
+      event.preventDefault(); // Prevents the default form submission behavior
+  
+      const sensorData = {
+        zoneId: sensorLocation, // Zone Id
+        sensorId: sensorId,
+        sensorName: sensorName,
+        sensorType: sensorType,
       };
+
+      fetch('https://automatic-space-acorn-rqp5pw5x9rxfppr9-5000.app.github.dev/sensor', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(sensorData),
+        })
+      .then(response => {
+        if (response.ok) {
+          Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: "Sensor has been added",
+            showConfirmButton: false,
+            timer: 1500
+          });
+          resetSensorState();
+        } else {
+          throw new Error('Failed to post sensor data');
+        }
+      })
+      .catch(error => {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: error.message,
+          confirmButtonColor: "#1f629c",
+        });
+      });
+    };
     return (
             <div id="datacontrol_page">
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="well">
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="well">
                         <h4>Add Zone</h4>
                         <form onSubmit={handleZoneSubmit}>
-                            <input type="text" 
+                            <input type="number" 
                                 value={zoneId} 
                                 onChange={e => setZoneId(e.target.value)} 
-                                placeholder='Zone Id'/>
+                                placeholder='Zone Id'
+                                required/>
                             <input type="text" 
                                 value={zoneName} 
                                 onChange={e => setZoneName(e.target.value)} 
-                                placeholder='Zone Name'/>
+                                placeholder='Zone Name'
+                                required/>
                             <input type="submit" value="Add Using REST API"/>
                         </form> 
                     </div>
                 </div>
-                <div class="col-sm-6">
-                    <div class="well">
+                <div className="col-sm-6">
+                    <div className="well">
                         <h4>Add Sensor</h4>
-                        {loading ? (
-                            <p>Loading...</p>
-                        ) : (
-                            <ul>
-                            {zoneData.length > 0 ? (zoneData.map((i) => <li key={i._id}>{i.zoneName}</li>)
-                            ) : (
-                                <li>No data available</li>
-                            )}
-                            <p>{loadingErr}</p>
-
-                            </ul>
-                        )}
-                        {/* <ul>
-                            {
-                                data.map( item => <li> {item.title} </li> )
-                            }
-                        </ul> */}
                         <form onSubmit={handleSensorSubmit}>
                             <select value={sensorLocation} onChange={e => setSensorLocation(e.target.value)} required>
-                            {zoneData.map((item) => (
-                                <option key={item._id} value={item.zoneId}>
-                                {item.zoneName}
-                                </option>
-                            ))
+                            <option value="" disabled>Select the zone</option>
+                            {
+                              zoneData.map((item) => (
+                                  <option value={item.zoneId}>{item.zoneName}</option>
+                              ))
                             }
-                                
                             </select>
-                            <input type="text" 
+                            <input type="number" 
                                 value={sensorId} 
                                 onChange={e => setSensorId(e.target.value)} 
-                                placeholder='Sensor Id'/>
+                                placeholder='Sensor Id'
+                                required/>
                             <input type="text" 
                                 value={sensorName} 
                                 onChange={e => setSensorName(e.target.value)} 
-                                placeholder='Sensor Name'/>
+                                placeholder='Sensor Name'
+                                required/>
                             <input type="text" 
                                 value={sensorType} 
                                 onChange={e => setSensorType(e.target.value)} 
-                                placeholder='Sensor Name'/>
+                                placeholder='Sensor Type'
+                                required/>
                             <input type="submit" value="Add Using REST API"/>
                         </form> 
                     </div>
                 </div>
             </div>
         </div>
-      );
+    );
 }
 
 function ErrorPage() {
--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ const express = require('express');
 const Data = require('./models/data');
 const Zone=require('./models/zone');
 const Sensor=require('./models/sensor');
+const bodyParser = require('body-parser');
+
 const MONGODB_URI =
   'mongodb+srv://coe558:kfupm@coe558.9j0xsua.mongodb.net/?retryWrites=true&w=majority';
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json()); // to parse the body from POST requests
 
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI)
@@ -18,70 +21,82 @@ mongoose.connect(MONGODB_URI)
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
-app.get('/getdata', async (req, res) => {
-    Data.find({}).then(function (data) {
-        res.json(data);
-    }).catch(function (err) {
-        res.json(err);
-    })
-})
-app.get('/getzone', async (req, res) => {
-    Zone.find({})
-    .then(data => res.json(data))
-    .catch(err => res.json(err))
-})
 
-app.post("/data", async (req, res) => {
+// REST APIs
+// Fetch All Records
+app.get('/getzones', async (req, res) => { // GET /zone
+    try{
+        const data = await Zone.find({})
+        res.send({status:"yes",data:data})
+    }catch(err){
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.get('/getsensors', async (req, res) => { // GET /sensor
+    try{
+        const data = await Sensor.find({})
+        res.send({status:"yes",data:data})
+    }catch(err){
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.get('/getdata', async (req, res) => { // GET /data
+    try{
+        const data = await Data.find({})
+        res.send({status:"yes",data:data})
+    }catch(err){
+        res.status(400).json({ message: err.message });
+    }
+});
+
+
+// POST Records
+app.post("/zone", async (req, res) => { // POST /zone
+    try{
+        const zoneId = req.body.zoneId;
+        const zoneName = req.body.zoneName;
+        const newZone = new Zone({ zoneId:zoneId, zoneName:zoneName });
+        await newZone.save();
+        // const newZoneId = newZone._id;
+        // res it added 
+        res.send(`zone with id ${newZone.zoneId} successfully  created`);
+    }catch(err){
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.post("/sensor", async (req, res) => { // POST /sensor
+    try{ 
+        const sensorId = req.body.sensorId;
+        const zoneId = req.body.zoneId;
+        const sensorType = req.body.sensorType;
+        const sensorName=req.body.sensorName;
+        const newSensor = new Sensor({sensorId:sensorId,zoneId:zoneId,sensorType:sensorType,sensorName:sensorName });
+        newSensor.save();
+        Zone.findOne({zoneId:zoneId}).then(
+            (Zone)=>{
+                Zone.sensors.push(newSensor._id);
+                Zone.save();
+            
+            })
+        res.send(`Sensor ${newSensor.sensorId} successfully  created`)
+    }catch(err){
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.post("/data", async (req, res) => { // POST /data
     try{
     const data=new Data({value:req.body.value,sensorId:req.body.sensorId})
     await data.save();
     res.status(201).json(data);
 }catch(err){
     res.status(400).json({ message: err.message });
-}})
+}});
 
-app.post("/zone", async (req, res) => {
-    try{
-    const zoneId = req.body.zoneId;
-    const zoneName = req.body.zoneName;
-    const newZone = new Zone({ zoneId:zoneId, zoneName:zoneName });
-    await newZone.save();
-    // res it added 
-    res.send(`zone ${newZone.zoneId} successfully  created`);
-    }catch(err){
-        res.status(400).json({ message: err.message });
-    }
-})
 
-app.post("/sensor", async (req, res) => {
-    try{ const sensorId = req.body.sensorId;
-        const zoneId = req.body.zoneId;
-        const sensorType = req.body.sensorType;
-        const sensorName=req.body.sensorName;
-        const newSensor = new Sensor({sensorId:sensorId,zoneId:zoneId,sensorType:sensorType,sensorName:sensorName });
-        await newSensor.save();
-        res.send(`Sensor ${newSensor.sensorId} successfully  created`)
-
-    }catch(err){
-        res.status(400).json({ message: err.message });}
-   
-})
-app.get('/getzones', async (req, res) => {
-    try{
-    const data = await Zone.find({})
-    res.send({status:"yes",data:data})
-   }catch(err){
-    res.status(400).json({ message: err.message });
-   }
-})
-
-app.get('/getsensors', async (req, res) => {
-    Sensor.find({}).then(function (data) {
-        res.json(data);
-    }).catch(function (err) {
-        res.json(err);
-    })
-})
 // find specific sensor by id
 app.post("/getsensor",async (req,res) =>{
     const sensorId=req.body.sensorId;
